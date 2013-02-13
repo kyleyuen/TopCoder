240fc14a0dbb83f019f73fbe0cf8d8aee54326bf
theblind <ruanbowen@gmail.com>
2013-02-13 14:40:56 +0800

    fix a problem

--- TopCoder/src/srm568/BallsSeparating.java
+++ TopCoder/src/srm568/BallsSeparating.java
@@ -2,122 +2,39 @@ package srm568;
 
 public class BallsSeparating {
 	public int minOperations(int[] red, int[] green, int[] blue) {
-		int n = red.length;
-		if (n < 3) {
-			return -1;
-		}
-		
-		int[] state = new int[n];
-		for (int i = 0; i < n; ++i) {
-			if (red[i] > green[i]) {
-				state[i] = 1;
-			}
-			else {
-				state[i] = 2;
-			}
-			if (blue[i] > Math.max(red[i], green[i])) {
-				state[i] = 3;
-			}
-		}
-		
-		while (true) {
-			int result = checkValid(state);
-			if (result == 0) {
-				break;
-			}
-			else {
-				fix(state, result, red, green, blue);
-			}
-		}
-		
-		int result = 0;
-		for (int i = 0; i < n; ++i) {
-			switch (state[i]) {
-			case 1:
-				result += green[i] + blue[i];
-				break;
-			case 2:
-				result += red[i] + blue[i];
-				break;
-			case 3:
-				result += red[i] + green[i];
-				break;
-			}
-		}
-		return result;
+		int INF = 1<<30; // A high value to determine impossible cases.
+	    // It is only impossible when there are less than 3 boxes.
+
+	    int res = INF;
+	    int n = red.length;
+	    for (int r = 0; r < n; r++) {
+	        for (int g = 0; g < n; g++) if (r != g) {
+	            for (int b = 0; b < n; b++) if (b != g && b != r) {
+	                int moves = 0;
+	                for (int i=0; i<n; i++) {
+	                    int cr = blue[i] + green[i]; //cost to make the box red
+	                    int cb = red[i] + green[i]; //cost to make blue
+	                    int cg = red[i] + blue[i]; //cost to make green
+	                    if (i == r) {
+	                        // must pick red:
+	                        moves += cr;
+	                    } else if (i == g) {
+	                        // must pick green:
+	                        moves += cg;
+	                    } else if (i == b) {
+	                        // must pick blue:
+	                        moves += cb;
+	                    } else {
+	                        // Pick the minimum cost:
+	                        moves += Math.min(cr, Math.min(cb,cg));
+	                    }
+	                }
+	                res = Math.min(res, moves);
+	            }
+	        }
+	    }
+
+	    return (res < INF) ? res : -1;
 	}
 
-	private void fix(int[] state, int result, int[] red, int[] green, int[] blue) {
-		// TODO Auto-generated method stub
-		boolean[] fixed = new boolean[4];
-		int[] counter = new int[4];
-		for (int i = 0; i < state.length; ++i) {
-			++counter[state[i]];
-		}
-		for (int i = 1; i <= 3; ++i) {
-			if (counter[i] == 1) {
-				fixed[i] = true;
-			}
-		}
-		
-		int index = 0;
-		int value = Integer.MAX_VALUE;
-		for (int i = 0; i < state.length; ++i) {
-			if (fixed[state[i]]) {
-				continue;
-			}
-			for (int j = 1; j <= 3; ++j) {
-				if (j != state[i]) {
-					continue;
-				}
-				
-				int temp;
-				if (result == 1) {
-					temp = red[i];
-				}
-				else if (result == 2) {
-					temp =green[i];
-				}
-				else {
-					temp = blue[i];
-				}
-				
-				switch (j) {
-				case 1:
-					if (value > red[i] - temp) {
-						value = red[i] - temp;
-						index = i;
-					}
-					break;
-				case 2:
-					if (value > green[i] - temp) {
-						value = green[i] - temp;
-						index = i;
-					}
-					break;
-				case 3:
-					if (value > blue[i] - temp) {
-						value = blue[i] - temp;
-						index = i;
-					}
-					break;
-				}
-			}
-		}
-		state[index] = result;
-	}
-
-	private int checkValid(int[] state) {
-		// TODO Auto-generated method stub
-		boolean[] flag = new boolean[4];
-		for (int i = 0; i < state.length; ++i) {
-			flag[state[i]] = true;
-		}
-		for (int i = 1; i <= 3; ++i) {
-			if (!flag[i]) {
-				return i;
-			}
-		}
-		return 0;
-	}
 }
